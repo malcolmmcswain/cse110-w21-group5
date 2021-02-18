2047873acdd295c8f29194cd304c21bc1b8e944e
Kevin Cao <k1cao@ucsd.edu>
2021-02-18 15:53:13 -0800

    added project list frontend

--- source/js/pomodoro.js
+++ source/js/pomodoro.js
@@ -5,13 +5,39 @@
  ************************************************************************************/
 
 // IDEAS/SUGGESTED STRUCTURE BELOW:
-    // This is to give you a starting point, not an explicit map of what your code should look like
+// This is to give you a starting point, not an explicit map of what your code should look like
 
 // Logical structure:
-    // Create a counter object to keep track of how many pomodoro's we've used
-    // Increment it based on some sort of signal of pomodoro completion from timer.js
-    // On each 3rd or 4th pomodoro, send some sort of signal to timer.js to switch to the "long break" state
+// Create a counter object to keep track of how many pomodoro's we've used
+// Increment it based on some sort of signal of pomodoro completion from timer.js
+// On each 3rd or 4th pomodoro, send some sort of signal to timer.js to switch to the "long break" state
 
 // UI structure:
-    // Fetch "pomodoro #" region from HTML using DOM
-    // Manipulate its text content to match that of the counter object state
+// Fetch "pomodoro #" region from HTML using DOM
+// Manipulate its text content to match that of the counter object state
+
+// Projects List Controls
+const hamburger = document.getElementById('hamburger');
+const projectList = document.getElementById('project-list');
+const modal = document.getElementById('modal');
+const addProject = document.getElementById('add-project');
+const closeAddProject = document.getElementById('close-add-project');
+
+hamburger.addEventListener('click', () => {
+    /* for some reason on load we must click twice for menu to show */
+    if (projectList.style.opacity === '0') {
+        projectList.style.opacity = '1';
+        projectList.style.pointerEvents = 'all';
+    } else {
+        projectList.style.opacity = '0';
+        projectList.style.pointerEvents = 'none';
+    }
+});
+
+addProject.addEventListener('click', () => {
+    modal.classList.add('open');
+});
+
+closeAddProject.addEventListener('click', () => {
+    modal.classList.remove('open');
+});
--- source/js/timer.js
+++ source/js/timer.js
@@ -1,5 +1,10 @@
 // TODO: Timer still has lag each second due to the lag of setInterval. Need to fix this. 
 
+const multipliers = {
+    SECOND: 1000,
+    MINUTE: 60000
+}
+
 /**
  * Represents a timer object
  * @constructor
@@ -7,68 +12,127 @@
  * @param {number} shortBreakMins - Length of short break timer
  * @param {number} longBreakMins - Length of long break timer
  */
-function timer(workMins = 25, shortBreakMins = 5, longBreakMins = 15) {
+function timer(workMins = 25, shortBreakMins = 5, longBreakMins = 15, longBreakInterval = 3) {
     this.state = 'reset';
+    this.counter = 0;
+
     this.countDownDate = null;
     this.countDownTimeout = null;
     this.terminateTimeout = null;
-    this.minutesLeft = 0;
-    this.secondsLeft = 0;
     this.workMins = workMins;
     this.shortBreakMins = shortBreakMins;
     this.longBreakMins = longBreakMins;
+
+    this.countDownMins = 0;
+    this.theta = 0;
+
+    // Used for internal testing
+    this.minutesLeft = 0;
+    this.secondsLeft = 0;
 }
 
 /**
  * Resets the count down timer
  */
-timer.prototype.reset = function() {
+timer.prototype.reset = function () {
+    this.stop(true);
+
+    this.counter = 0;
     this.state = 'reset';
+
     this.countDownDate = null;
     this.countDownTimeout = null;
     this.terminateTimeout = null;
     this.minutesLeft = 0;
     this.secondsLeft = 0;
+    this.countDownMins = 0;
+    this.theta = 0;
+
+    document.getElementById('time').innerHTML = '00:00';
+    document.getElementById('dark').setAttribute('d', 'M60,60 v-60 a60,60 0 0,1 0,0');
 }
 
 /**
  * Stops the count down timer
  */
-timer.prototype.stop = function() {
-    this.state = 'stopped';
+timer.prototype.stop = function (force = false) {
     if (this.countDownTimeout) clearInterval(this.countDownTimeout);
-    if (this.terminateTimeout) clearTimeout(this.terminateTimeout);
+
+    console.log('stopped in state ' + this.state);
+
+    if (force) {
+        this.state = 'stopped';
+    } else if (this.state == 'work' && this.counter % this.longBreakInterval == 0) {
+        this.reset();
+        this.startLongBreak();
+    } else if (this.state == 'work') {
+        this.reset();
+        this.startShortBreak();
+    } else if (this.state == 'short_break') {
+        this.reset();
+        this.startWorking();
+    }
+
+    /* 
+     * Terminate automatic stop. Needed when user terminates timer
+     * early to prevent an incorrect automatic stop when a new timer
+     * is started.
+     */
+    // if (this.terminateTimeout) clearTimeout(this.terminateTimeout);
 }
 
 /**
  * Start countDown based on passed in param countDownMins
  * @param {number} countDownMins count down time in minutes
  */
-timer.prototype.start = function(countDownMins) {
-    this.stop();
+timer.prototype.start = function (countDownMins) {
     let now = new Date();
-    this.countDownDate = new Date(now.getTime() + countDownMins * 60 * 1000 + 1000);
-    this.countDownTimeout = setInterval(this.countDown.bind(this), 1000);
-    this.terminateTimeout = setTimeout(function() {
-        clearInterval(this.countDownTimeout);
-    }.bind(this), countDownMins * 60 * 1000 + 1000); // + 1s so we get 00:00 print
+    let countDownOffset = countDownMins * multipliers.MINUTE;
+    this.countDownDate = new Date(now.getTime() + countDownOffset);
+    this.countDownMins = countDownMins;
+
+    /* Neat hack to prevent lag of the first second */
+    this.countDown.bind(this)();
+    this.countDownTimeout = setInterval(this.countDown.bind(this), 500);
 }
 
 /**
  * Update timeLeft based on current time and countDownDate
  */
-timer.prototype.countDown = function() {
+timer.prototype.countDown = function () {
+    function timerPad(timerField) {
+        return timerField.toString().padStart(2, '0');
+    }
+
     let now = new Date();
-    let timeLeft = this.countDownDate.getTime() - now.getTime();
-    this.minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    this.secondsLeft = (timeLeft % (1000 * 60)) / 1000;
-    console.log(`${this.minutesLeft.toString().padStart(2,'0')}:${this.secondsLeft.toString().padStart(2,'0')}`);
+    // Use seconds that have passed as the single source of truth.
+    let timeLeft = Math.ceil((this.countDownDate - now) / multipliers.SECOND);
+    this.minutesLeft = Math.floor(timeLeft / 60);
+    this.secondsLeft = timeLeft % 60;
+
+    let dark = document.getElementById('dark'),
+        radius = document.getElementById('svg').getBBox().width / 2;
+    dark.setAttribute('transform', 'translate(' + radius + ',' + radius + ')');
+
+    let increment = 360 / (this.countDownMins * 60) / 2;
+    this.theta += increment;
+    let d = 'M0,0 v' + -radius + 'A' + radius + ' ' + radius + ' 1 ' + ((this.theta > 180) ? 1 : 0) + ' 1 ' + Math.sin(this.theta * Math.PI / 180) * radius + ' ' + Math.cos(this.theta * Math.PI / 180) * -radius + 'z';
+    dark.setAttribute('d', d);
+
+    document.getElementById('time').innerHTML = `${timerPad(this.minutesLeft)}:${timerPad(this.secondsLeft)}`;
+    if (!this.minutesLeft && !this.secondsLeft) {
+        clearInterval(this.countDownTimeout);
+        this.stop();
+    }
 }
 
 /**
  * Start work timer
  */
-timer.prototype.startWorking = function() {
+timer.prototype.startWorking = function () {
+    this.counter++;
+    document.getElementById('light').setAttribute('fill', '#E46E6E');
+    document.getElementById('time').setAttribute('fill', '#E46E6E');
     this.state = 'work';
     this.start(this.workMins);
 }
@@ -76,7 +140,9 @@ timer.prototype.startWorking = function() {
 /**
  * Start short break timer
  */
-timer.prototype.startShortBreak = function() {
+timer.prototype.startShortBreak = function () {
+    document.getElementById('light').setAttribute('fill', '#6FEA9A');
+    document.getElementById('time').setAttribute('fill', '#6FEA9A');
     this.state = 'short_break';
     this.start(this.shortBreakMins);
 }
@@ -84,7 +150,9 @@ timer.prototype.startShortBreak = function() {
 /**
  * Start long break timer
  */
-timer.prototype.startLongBreak = function() {
+timer.prototype.startLongBreak = function () {
+    document.getElementById('light').setAttribute('fill', '#6FEA9A');
+    document.getElementById('time').setAttribute('fill', '#6FEA9A');
     this.state = 'long_break';
     this.start(this.longBreakMins);
 }
@@ -96,8 +164,22 @@ timer.prototype.resume = () => {
     this.start((timeLeft) / (60 * 1000));
 } */
 
-// UI structure:
-    // Fetch timer from HTML using DOM
-    // Manipulate its text content to match that of the timer object state
-    // Style its "ring" conditionally (this part will be tricky, ask me if you need help!)
-module.exports = timer;
+document.getElementById('start').addEventListener('click', () => {
+    let time = new timer(1, 1, 1);
+    time.startWorking();
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('stop').style.display = 'block';
+    document.getElementById('stop').addEventListener('click', () => {
+        time.stop(true);
+    });
+    document.getElementById('reset').style.display = 'block';
+    document.getElementById('reset').addEventListener('click', () => {
+        time.reset();
+        document.getElementById('start').style.display = 'block';
+        document.getElementById('stop').style.display = 'none';
+        document.getElementById('reset').style.display = 'none';
+    });
+});
+
+
+// module.exports = timer;
