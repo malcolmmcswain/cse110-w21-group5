3dabead3c54367575efed25455a4268d50d87095
AxiosDeminence <juhmer@att.net>
2021-02-18 21:59:00 -0800

    Update unit tests for decoupled timer

--- tests/timer.test.js
+++ tests/timer.test.js
@@ -7,61 +7,115 @@ const MULTIPLIERS = {
     minutes: 60000,
 }
 
+// Using modern fake timers allows date functions to be mocked and settimeout functions to be mocked.
 jest.useFakeTimers('modern');
 
+let pomo;
+
+beforeEach(() => {
+    // Create a fresh document before each test.
+    document.body.innerHTML = `
+    <svg id="timer-display" viewBox="0 0 80 80">
+        <g id="rings">
+            <circle cx="40" cy="40" r="36" fill="none" id="background-ring"/>
+            <circle cx="40" cy="40" r="36" fill="none" id="burndown-ring" stroke-dasharray="0 226">
+                <!-- SVG to animate the stroke of the circle. Started and duration edited by timer.js -->
+                <animate id="burndown-anim" attributeType="XML" attributeName="stroke-dasharray" from="0 226" to="226.08 226" begin="indefinite"/>
+            </circle>
+        </g>
+        <text id="time" x="40" y="48" font-size="20px" text-anchor="middle" fill="#E46E6E">00:00</text>
+    </svg>
+
+    <!-- The text for session counter would be replaced once user starts working -->
+    <text id="session-counter">Start your working session now</text>  
+  
+    <div id="timer-control">
+        <button id="start">Start</button>
+        <button id="stop">Stop</button>
+        <button id="reset">Reset</button>
+    </div>`;
+
+    // Get all elements used to pass into pomodoro
+    let timeDisplay = document.getElementById('time');
+    let backgroundRing = document.getElementById('background-ring');
+    let burndownRing = document.getElementById('burndown-ring');
+    let burndownAnim = document.getElementById('burndown-anim');
+    let sessionCounter = document.getElementById('session-counter');
+
+    // Mock SVGElement functions as JSDOM does not support SVGElement functions.
+    const mockFn = jest.fn(() => undefined);
+    burndownAnim.beginElement = mockFn;
+    burndownAnim.endElement = mockFn;
+    burndownAnim.ownerSVGElement.unpauseAnimations = mockFn;
+    burndownAnim.ownerSVGElement.pauseAnimations = mockFn;
+
+    pomo = new timer(timeDisplay, backgroundRing, burndownRing, burndownAnim, sessionCounter);
+});
+
 test('Timer resets properly', () => {
-    let time = new timer();
-    time.reset();
-    expect(time.minutesLeft).toBe(0);
+    pomo.reset();
+    expect(pomo.minutesLeft).toBe(0);
 });
 
 test('Timer initializes working session properly', () => {
-    let time = new timer();
-    time.startWorking();
-    expect(time.minutesLeft).toBe(25);
-    expect(time.secondsLeft).toBe(0);
+    pomo.startWorking();
+    expect(pomo.minutesLeft).toBe(25);
+    expect(pomo.secondsLeft).toBe(0);
 });
 
+
+// Ensures that the timer begins counting down properly
 test('Timer starts working properly', () => {
-    let time = new timer();
-    time.startWorking();
+    pomo.startWorking();
     jest.advanceTimersByTime(1000);
-    expect(time.minutesLeft).toBe(24);
-    expect(time.secondsLeft).toBe(59);
+    expect(pomo.minutesLeft).toBe(24);
+    expect(pomo.secondsLeft).toBe(59);
 });
 
+// Ensures that the timer counts down minutes properly
 test('Timer counts down properly', () => {
-    let time = new timer();
-    time.startWorking();
+    pomo.startWorking();
     jest.advanceTimersByTime(61000);
-    expect(time.minutesLeft).toBe(23);
-    expect(time.secondsLeft).toBe(59);
+    expect(pomo.minutesLeft).toBe(23);
+    expect(pomo.secondsLeft).toBe(59);
 });
 
+
+// Ensures that the timer does not progress when manually stopped
 test('Timer manually stops properly', () => {
-    let time = new timer();
-    time.startWorking();
+    pomo.startWorking();
     jest.advanceTimersByTime(1000);
-    time.stop(true);
+
+    pomo.stop(true);
     jest.advanceTimersByTime(61000);
-    expect(time.minutesLeft).toBe(24);
-    expect(time.secondsLeft).toBe(59);
+    expect(pomo.minutesLeft).toBe(24);
+    expect(pomo.secondsLeft).toBe(59);
 });
 
 test('Timer automatically stops properly', () => {
-    let time = new timer();
-    time.startWorking();
-    jest.advanceTimersByTime(time.workMins * MULTIPLIERS.minutes - MULTIPLIERS.seconds);
-    expect(time.state).toBe('work');
-    expect(time.minutesLeft).toBe(0);
-    expect(time.secondsLeft).toBe(1);
-    jest.advanceTimersByTime(MULTIPLIERS.seconds);
-    expect(time.state).toBe('short_break');
-    expect(time.minutesLeft).toBe(5);
-    expect(time.secondsLeft).toBe(0);
-    jest.advanceTimersByTime(MULTIPLIERS.minutes * (time.workMins * 3 + time.shortBreakMins * 3));
-    expect(time.counter).toBe(4);
-    expect(time.state).toBe('long_break');
-    expect(time.minutesLeft).toBe(15);
-    expect(time.secondsLeft).toBe(0);
+    pomo.startWorking();
+
+    // Advance through phases to reach long break and then reach back to short breaks
+    for (let i = 1; i < 6; ++i) {
+        // Advance through work
+        expect(pomo.counter).toBe(i);
+        expect(pomo.state).toBe('work');
+        expect(pomo.minutesLeft).toBe(pomo.workMins);
+        expect(pomo.secondsLeft).toBe(0);
+        jest.advanceTimersByTime(pomo.workMins * MULTIPLIERS.minutes);
+
+        if (i % 4 == 0) { // Do long break if on long break interval
+            expect(pomo.counter).toBe(pomo.longBreakInterval);
+            expect(pomo.state).toBe('long_break');
+            expect(pomo.minutesLeft).toBe(15);
+            expect(pomo.secondsLeft).toBe(0);
+            jest.advanceTimersByTime(pomo.longBreakMins * MULTIPLIERS.minutes);
+        } else { // Do short break if not on long break interval
+            expect(pomo.counter).toBe(i);
+            expect(pomo.state).toBe('short_break');
+            expect(pomo.minutesLeft).toBe(pomo.shortBreakMins);
+            expect(pomo.secondsLeft).toBe(0);
+            jest.advanceTimersByTime(pomo.shortBreakMins * MULTIPLIERS.minutes);
+        }
+    }
 });
