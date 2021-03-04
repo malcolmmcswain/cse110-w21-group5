a2a3dae8e3558cafb97440b9961dcd9f12234ab5
Rui Cai <rucai@ucsd.edu>
2021-03-04 00:15:03 -0800

    Minor change

--- source/cypress/plugins/index.js
+++ source/cypress/plugins/index.js
@@ -20,11 +20,11 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 }
 
-module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
-  // include any other plugin code...
+// module.exports = (on, config) => {
+//   require('@cypress/code-coverage/task')(on, config)
+//   // include any other plugin code...
 
-  // It's IMPORTANT to return the config object
-  // with any changed environment variables
-  return config
-}
+//   // It's IMPORTANT to return the config object
+//   // with any changed environment variables
+//   return config
+// }
