a26d2463bf55330a8c02b73df4ae7a1694bea058
Malcolm McSwain <mcswain1@pacbell.net>
2021-03-04 17:38:27 -0800

    Fix merge conflicts & cleanup

--- source/js/pomodoro.js
+++ source/js/pomodoro.js
@@ -50,27 +50,31 @@ function initializePage() {
     let editProjectName = document.getElementById('edit-project-name');
     let editProject     = document.getElementById('edit-project');
 
+    // Pomodoro Options
     let pomLength       = document.getElementById('pom-length');
     let shortLength     = document.getElementById('short-length');
     let longLength      = document.getElementById('long-length');
+    let saveOptions     = document.getElementById('save-options');
 
 
     // Load in previous options (default without loading is 25/5/30)
-    if(localStorage.getItem('pomLength') != null)
+    if (localStorage.getItem('pomLength') != null)
         pomLength.value = localStorage.getItem('pomLength');
-    if(localStorage.getItem('shortLength') != null)
+    else pomLength.value = 25;
+    
+    if (localStorage.getItem('shortLength') != null)
         shortLength.value = localStorage.getItem('shortLength');
-    if(localStorage.getItem('longLength') != null)
+    else shortLength.value = 5;
+
+    if (localStorage.getItem('longLength') != null)
         longLength.value = localStorage.getItem('longLength');
+    else longLength.value = 30;
 
     // Update storage on options edit
-    pomLength.addEventListener('input', e=> {
+    saveOptions.addEventListener('click', e => {
+        e.preventDefault();
         localStorage.setItem('pomLength', pomLength.value);
-    });
-    shortLength.addEventListener('input', e=> {
         localStorage.setItem('shortLength', shortLength.value);
-    });
-    longLength.addEventListener('input', e=> {
         localStorage.setItem('longLength', longLength.value);
     });
     
@@ -101,9 +105,9 @@ function initializePage() {
 
     startBtn.addEventListener('click', e => {
         // To be replaced with grabbing from settings menu
-        window.time.workMins = 6/60;
-        window.time.shortBreakMins = 6/60;
-        window.time.longBreakMins = 6/60;
+        window.time.workMins = parseInt(pomLength.value);
+        window.time.shortBreakMins = parseInt(shortLength.value);
+        window.time.longBreakMins = parseInt(longLength.value);
 
         // Begin working and display stop/reset buttons
         window.time.startWorking();
